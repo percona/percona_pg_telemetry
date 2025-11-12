58fa255eed9e66eefde8394aa911c3f601609917
Andreas Karlsson <andreas.karlsson@percona.com>
2025-11-12 16:35:52 +0100

    PG-2010 Replace project with a stub
    We plan to, at least temporarily, retire this project due to lack of
    faith in the quality of the the data it gathers, and to do that safely
    without breaking people's installations we need to replace it with a
    dummy module which does as little as possible.
    
    The reason for this is because this extension is loaded via
    shared_preload_libraries and if you just remove the extension from
    people's computers when they upgrade Percona's PostgreSQL distribution
    then PostgreSQL will fail to start after upgrading. So we will still
    need to ship a shared library and to minimize maintenance we make it do
    as little as possible.
    
    This commit removes all logic expect the two SQL facing functions
    exposed in the extension.

--- sql/basic.sql
+++ sql/basic.sql
@@ -1,20 +1,8 @@
---
--- basic sanity
---
-
 CREATE  EXTENSION percona_pg_telemetry;
-
--- Sleep so that waiting on agent becomes true
-SELECT  pg_sleep(3);
-
-SELECT  name
-FROM    pg_settings
-WHERE   name LIKE 'percona_pg_telemetry.%';
 
 SELECT  percona_pg_telemetry_version();
 
-SELECT  regexp_replace(regexp_replace(latest_output_filename, '\d{11,}', '<INSTANCE ID>', 'g'), '\d{6,}', '<TIMESTAMP>', 'g') AS latest_output_filename
-        , pt_enabled
+SELECT  *
 FROM    percona_pg_telemetry_status();
 
 DROP    EXTENSION percona_pg_telemetry;
